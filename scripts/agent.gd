692cfd3df7eb719bd0ea8cc8c4f6b0066a756242
Alex Shopov <shopov.alex@gmail.com>
2025-04-13 21:13:20 -0400

    added wander, behaviors extend steering base class with abstract get_steering method

--- scripts/agent.gd
+++ scripts/agent.gd
@@ -4,13 +4,17 @@ extends CharacterBody2D
 @export var speed := 200
 @export var rotation_speed := 5.
 
-var kinematic_seek: KinematicSeek
-var kinematic_arrive: KinematicArrive
+var kinematic: KinematicSteering
+var screen_size: Vector2
 var target: Vector2 = Vector2.ZERO
 
 func _init() -> void:
-	kinematic_seek = KinematicSeek.new(self, speed)
-	kinematic_arrive = KinematicArrive.new(self, speed, 10)
+	# kinematic= KinematicSeek.new(self, speed)
+	# kinematic= KinematicArrive.new(self, speed, 10)
+	kinematic= KinematicWander.new(self, speed, PI)
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"click"):
@@ -20,8 +24,7 @@ func _physics_process(delta: float) -> void:
 	if target == Vector2.ZERO:
 		return
 
-	# var steering: KinematicSteeringOutput = kinematic_seek.get_steering(target)
-	var steering: KinematicSteeringOutput = kinematic_arrive.get_steering(target)
+	var steering: KinematicSteeringOutput = kinematic.get_steering(target)
 	if not steering:
 		return
 
@@ -29,3 +32,6 @@ func _physics_process(delta: float) -> void:
 	rotation = lerp_angle(rotation, steering.rotation, rotation_speed * delta)
 
 	move_and_slide()
+
+	position.x = fposmod(position.x, screen_size.x)
+	position.y = fposmod(position.y, screen_size.y)
--- scripts/steering/kinematic_steering.gd
+++ scripts/steering/kinematic_steering.gd
@@ -4,3 +4,7 @@ var agent: Agent
 
 func _init(p_agent):
     agent = p_agent
+
+# child classes will override this to implement steering behaviour
+func get_steering(_target: Vector2) -> KinematicSteeringOutput:
+    return null
